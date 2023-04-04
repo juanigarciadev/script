d4afd7783dba799ae3c1801716d2d22ba17d6a1d
Juan Ignacio Garcia <nachogarcia.1017@gmail.com>
2023-04-03 21:11:51 -0300

    fixed logic container in 'ItemList' component

--- src/App.js
+++ src/App.js
@@ -8,30 +8,11 @@ import { ItemListContainer } from "./components/ItemList/ItemListContainer";
 import { useState } from "react";
 
 function App() {
-  //Saludo
-  let greetings = "Hello, welcome again!";
-
-  //Logica contador
-  const [quantity, setQuantity] = useState(0);
-
-  function sumar() {
-    setQuantity(quantity + 1);
-  }
-
-  function restar() {
-    setQuantity(quantity - 1);
-  }
-
   return (
     <div className="App">
       <Navbar />
       <Home />
-      <ItemListContainer
-        greetings={greetings}
-        sumar={sumar}
-        restar={restar}
-        quantity={quantity}
-      />
+      <ItemListContainer />
       <Trusted />
       <Counter />
       <Footer />
--- src/components/ItemList/ItemListContainer.jsx
+++ src/components/ItemList/ItemListContainer.jsx
@@ -1,6 +1,20 @@
 import { ItemList } from "./ItemList";
+import { useState } from "react";
 
-export const ItemListContainer = ({ greetings, sumar, restar, quantity }) => {
+export const ItemListContainer = () => {
+  //Saludo
+  let greetings = "Hello, welcome again!";
+
+  //Logica contador
+  const [quantity, setQuantity] = useState(0);
+
+  function sumar() {
+    setQuantity(quantity + 1);
+  }
+
+  function restar() {
+    setQuantity(quantity - 1);
+  }
   return (
     <ItemList
       greetings={greetings}
