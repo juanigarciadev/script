4bc884c0213d92d2bf9bbcfcc01152cdb937a412
Juan Ignacio Garcia <nachogarcia.1017@gmail.com>
2023-05-13 01:47:13 -0300

    added some regex to form checkout validation

--- src/components/FormCheckout/FormCheckoutContainer.jsx
+++ src/components/FormCheckout/FormCheckoutContainer.jsx
@@ -61,24 +61,29 @@ const FormCheckoutContainer = () => {
       firstName: Yup.string()
         .min(3, "Minimum 3 characters")
         .max(16, "Maximum 16 characters")
+        .matches("[a - zA - Z]", "This field must be letters")
         .required("This field is required"),
       lastName: Yup.string()
         .min(3, "Minimum 3 characters")
         .max(16, "Maximum 16 characters")
+        .matches("[a - zA - Z]", "This field must be letters")
         .required("This field is required"),
       email: Yup.string()
         .email("This field must be an valid email")
         .required("This field is required"),
-      country: Yup.string().required("This field is required"),
-      postalCode: Yup.number("This field must be an number").required(
-        "This field is required"
-      ),
-      phone: Yup.number("This field must be an number").required(
-        "This field is required"
-      ),
       confirmEmail: Yup.string()
         .required("This field is required")
         .oneOf([Yup.ref("email")], "The e-mails do not match"),
+      country: Yup.string()
+        .required("This field is required")
+        .min(3, "Minimum 3 characters")
+        .max(20, "Maximum 20 characters"),
+      postalCode: Yup.number("This field must be an number")
+        .typeError("This field must be a number")
+        .required("This field is required"),
+      phone: Yup.number("This field must be a number")
+        .typeError("This field must be a number")
+        .required("This field is required"),
     }),
     validateOnChange: false,
   });
